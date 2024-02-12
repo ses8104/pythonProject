532eea7ca9d0735849d9cc0e7dccd1c28a45e477
Alvin <ses8104@naver.com>
2024-02-12 14:16:48 +0900

    add greed3-2

--- greed3-2.py
+++ greed3-2.py
@@ -1,3 +1,10 @@
+# 값을 입력하세요 공백을 이용해 여러개의 수를 입력 가능> (가장큰수의 사용횟수, 더하기 반복횟수)
+# 8 3
+# 2 4 5 4 6
+# 결과값 :  46
+#
+# Process finished with exit code 0
+
 print("값을 입력하세요 공백을 이용해 여러개의 수를 입력 가능> (가장큰수의 사용횟수, 더하기 반복횟수)")
 
 numMaxRepeat, addRepeatCount = map(int, input().split())
@@ -8,20 +15,10 @@ first  = data[len(data) - 1]
 second = data[len(data) - 2]
 
 result = 0
+count = int(numMaxRepeat / (addRepeatCount + 1)) * addRepeatCount
+count += numMaxRepeat % (addRepeatCount + 1)
 
-while True:
-    for i in range(addRepeatCount):
-
-        if numMaxRepeat == 0:
-            break
-
-        result += first
-        numMaxRepeat -= 1
-
-    if numMaxRepeat == 0:
-        break
-
-    result += second
-    numMaxRepeat -= 1
+result += (count) * first
+result += (numMaxRepeat - count) * second
 
 print('결과값 : ', result)
